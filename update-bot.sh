a9120f6d33cb32634919e1645c2fb3e18df6c041
Gareth Evans <gareth@bryncynfelin.co.uk>
2020-03-13 20:36:14 +0000

    chore: push updates into the instance template

--- update-bot.sh
+++ update-bot.sh
@@ -4,5 +4,6 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-jx step create pr regex --regex "\s*VERSIONS_GIT_REF=\"(.*)\"" --version "v${VERSION}" --files "templates/.secrets.defaults" --repo https://github.com/cloudbees/arcalos
+jx step create pr regex --regex "variable\s\"request_versions_git_ref\"\s{\n\s\sdefault\s\s\s\s\s=\s\"(.*)\"" --version "v${VERSION}" --files "workspace-template/variables.tf" --repo https://github.com/cloudbees/arcalos-instance-template
+
 sed 's:^.*\[jenkins-x/jenkins-x-builders\].*\[\([0-9.\-]*\)\].*$:\1:;t;d' dependency-matrix/matrix.md
